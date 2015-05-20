c13047d4b20445613e02d33f151f1b5dd1f49893
yurongde <359067441@qq.com>
2015-05-20 08:11:29 +0800

    增加水平等高的一级view

---
+++ SDPureLayoutPractice/Demos/SDPureLayoutDemo3ViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDPureLayoutDemo3ViewController.h
+//  SDPureLayoutPractice
+//
+//  Created by yurongde on 15/5/20.
+//  Copyright (c) 2015年 yurongde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDPureLayoutDemo3ViewController : UIViewController
+
+@end
